b1964b208a95de12cadf87432dbe5fbd47eaac87
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-04-03 11:38:41 +0200

    Updated license name

--- message_response.go
+++ message_response.go
@@ -4,6 +4,7 @@ type response[T message, R any] interface {
     message[T, R, ResponseCookie]
 
     GetStatusCode() uint16
+    WithStatusCode(code uint16) (T, error)
     WithStatus(code uint16, reasonPhrase string) (T, error)
     GetReasonPhrase() string
 }
